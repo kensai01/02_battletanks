66b563bbc9d863d9d1355a42599387566ca9f169
kensai01 <br9975@wayne.edu>
2016-11-23 15:45:34 -0500

    First iteration circle done. Working on the tank again. This commit declares a few classes that will allow aiming and general AI. Also, fixed menu exit problem.

---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTanks.h"
+#include "TankPlayerController.h"
+
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+}
---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" //must be the last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const; 
+	
+};
