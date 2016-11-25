a9623eb012ec6266f5c18069d25b521041a6699e
kensai01 <br9975@wayne.edu>
2016-11-25 18:30:18 -0500

    Fixed bug that made tanks pop around. Also, spawn projectile at barrel end.

--- BattleTanks/Source/BattleTanks/Private/Tank.cpp
+++ BattleTanks/Source/BattleTanks/Private/Tank.cpp
@@ -3,16 +3,28 @@
 #include "BattleTanks.h"
 #include "../Public/TankAimingComponent.h"
 #include "../Public/Tank.h"
+#include "../Public/Projectile.h"
+#include "../Public/TankBarrel.h"
 
-void ATank::Fire() 
+void ATank::Fire()
 {
 	auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("%f: Tank Fires!"), Time);
+
+	if (!Barrel) { return; }
+
+	// Spawn a projectile at the socket location on the barrel
+	GetWorld()->SpawnActor<AProjectile>(
+		ProjectileBlueprint,
+		Barrel->GetSocketLocation(FName("Projectile")),
+		Barrel->GetSocketRotation(FName("Projectile"))
+		);
 }
 
 void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
 {
 	TankAimingComponent->SetBarrelReference(BarrelToSet);
+	Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret * TurretToSet)
