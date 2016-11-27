d98531019477d2750bf4ade8fbba00ccf3241cad
kensai01 <br9975@wayne.edu>
2016-11-27 10:56:10 -0500

    Tank movement done, AI movement done. Class cleaned up. Bug exists where tank hits something in the floor and flies up, happens FAR too often the tank can't even go more than 10 meters before flipping.

---
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -0,0 +1,45 @@
+// Copyright Mirza Besic 2016
+
+#include "BattleTanks.h"
+#include "../Public/TankTrack.h"
+#include "../Public/TankMovementComponent.h"
+
+/** path following: request new velocity */
+void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
+{
+	/// COMMENT BLOCK
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//auto Name = GetName();
+	//auto TankName = GetOwner()->GetName();
+
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	IntendMoveForward(ForwardThrow);
+
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
+
+	//UE_LOG(LogTemp, Warning, TEXT("ForwardThrow: %f RightThrow: %f"), ForwardThrow, RightThrow);
+}
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
+
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+}
